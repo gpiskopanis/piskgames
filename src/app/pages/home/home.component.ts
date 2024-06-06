179e31bcd444bb8b2083fb75d05d129f04d1c70c
gpiskopanis <gpiskopanis@gmail.com>
2024-06-06 22:42:14 +0300

    one

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../../../src/app/services/api_service.service';
+import { ApiService } from '../../services/api_service.service';
 
 @Component({
   selector: 'app-home',
