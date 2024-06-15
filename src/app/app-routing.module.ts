124b52a9e6fc5b9acc522a2aa8ab4739894dc5dc
gpiskopanis <gpiskopanis@gmail.com>
2024-06-15 14:46:50 +0300

    added Api to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { IcrepoComponent } from './pages/icrepo/icrepo.component';
 import { IcollabComponent } from './pages/icollab/icollab.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ScriptHackComponent } from './scripthack/scripthack.component';
+import { ApiService } from './services/api_service.service';
 
 
 const routes: Routes = [
+  {path:'Api',component:ApiService},
   {path:'menu',component:MenuComponent},
   {path:'icrepo',component:IcrepoComponent},
   {path:'icollab',component:IcollabComponent},
