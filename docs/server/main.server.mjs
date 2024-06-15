134d3bc1a5f903730a4a28de3c1e36634554ce13
gpiskopanis <gpiskopanis@gmail.com>
2024-06-15 14:17:41 +0300

    added the service cashe control

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3YZ72FPZ.mjs";import"./chunk-LXRSBSLW.mjs";import"./chunk-LL5F25PN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ND4MD6P3.mjs";import"./chunk-FIVMY32M.mjs";import"./chunk-LL5F25PN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
