155e9106306b29cc532cc6663937b2d81660f329
gpiskopanis <gpiskopanis@gmail.com>
2024-06-16 23:45:18 +0300

    assets are back

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RUMBDQB4.mjs";import"./chunk-DMEJ2AXG.mjs";import"./chunk-LL5F25PN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-UYCV4V7K.mjs";import"./chunk-DMEJ2AXG.mjs";import"./chunk-LL5F25PN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
